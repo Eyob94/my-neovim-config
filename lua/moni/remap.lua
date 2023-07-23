1a07b3d25a63611bc97a8b709407f3cb51efce21
Eyob94 <eyobsh123@gmail.com>
2023-07-23 12:45:47 +0300

    :wrench: add set file for configuring vim options

--- lua/moni/remap.lua
+++ lua/moni/remap.lua
@@ -1,16 +1,22 @@
-vim.g.mapleader = " "
-
 -- .netrw configs
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
-
-vim.keymap.set('i', '<A-C>', '<ESC>') 
-vim.keymap.set('v', '<A-C>', '<ESC>')
+-- Mode Navigation
+vim.keymap.set('n', ';;', '<ESC>')
+vim.keymap.set('v', ';;', '<ESC>')
 
 -- Navigate buffers
 vim.keymap.set('n', '<S-l>', ':bnext<CR>')
 vim.keymap.set('n', '<S-h>', ':bprevious<CR>')
 
+--Code navigation
+vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv')
+vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv')
+
 -- Nvim Tree Keymaps
 vim.keymap.set('n', '<C-b>', ':NvimTreeToggle<CR>')
 
+
+--CLipboard remaps
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>p', '"+p')
